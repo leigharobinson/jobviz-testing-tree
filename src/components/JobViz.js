940a6d727c7814b3c9aa4bfb4175561e23bf8361
leigharobinson <lmarobinson24@gmail.com>
2020-12-02 13:46:46 -0600

    first commit

---
+++ src/components/JobViz.js
@@ -0,0 +1,12 @@
+import React from "react"
+import "./JobViz.css"
+
+export const JobViz = () => (
+    <>
+        <h2>Job Viz</h2>
+        <small>What do you want to be?</small>
+        <address>
+            
+        </address>
+    </>
+)
