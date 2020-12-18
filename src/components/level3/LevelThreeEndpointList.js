6f550a209b5380f9a6c0ac0aaaf13ce943fa66df
leigharobinson <lmarobinson24@gmail.com>
2020-12-18 12:43:50 -0600

    refactored Level Three Endpoint to have better wirtten code

--- src/components/level3/LevelThreeEndpointList.js
+++ src/components/level3/LevelThreeEndpointList.js
@@ -1,198 +1,224 @@
 import React, { useState, useEffect } from "react";
-import JobManager from "../../modules/JobManager"
-import {Title} from "../title/Title"
-import "../styling/Style.css"
-import {LevelThreeCard} from "./LevelThreeCard"
-import {makeStringPath, removeDash} from "../Helper"
+import JobManager from "../../modules/JobManager";
+import { Title } from "../title/Title";
+import "../styling/Style.css";
+import { LevelThreeCard } from "./LevelThreeCard";
+import { makeStringPath, removeDash } from "../Helper";
 import { Link } from "react-router-dom";
-import {Table} from "../table/Table"
-// import { Autocomplete } from "../search/OldSearch"
-// import {LrAutoSearch} from "../search/LRautoSearch"
-import {LrAutoSearchV2} from "../search/LRautoSearchV2"
+import { Table } from "../table/Table";
+
+import { LrAutoSearchV2 } from "../search/LRautoSearchV2";
 
 export const LevelThreeEndpointList = (props) => {
-    //All the objects in jobs array
-    const [jobs, setJobs] = useState([]);
-    //where we set the id of the category that was clicked to sate
-    const [jobName, setJobName] = useState([]);
-    const [jobObj, setJobObj] = useState({
-        id: 0,
-        title: "",
-        Hierarchy: "",
-        OccupationType: "",
-        Employment2016: 0,
-        Employment2026: 0,
-        ChgEmploy2016to26Num: 0,
-        ChgEmploy2016to26Perc: 0,
-        PercentSelfEmployed2016: 0,
-        OccupationalOpenings2016to2026AnnualAverage: 0,
-        MedianAnnualWage2017: "",
-        TypicalEducationNeededForEntr: "",
-        WorkExperienceInARelatedOccupation: "",
-        TypicalOnTheJobTrainingNeededToAttainCompetencyInTheOccupation: "",
-        ttl: "",
-        Level0: "",
-        Level4: "",
-        Level3: "",
-        Level2: "",
-        Level1: "",
-        pathString: "",
-        Def: "",
-    
-        })
-    //endpoint
-    let endpoint = props.category
-    // the level 1 category url sting we need to pass to children
-    const levelOneUrl = props.levelOneUrl;
-    // the level 2 category url sting we need to pass to children
-    const levelTwoUrl = props.levelTwoUrl;
-       //we only select level 2 categories that have the same level 1 category
-    let levelOneString = makeStringPath(levelOneUrl);
-     //the level 2 category 'normal' string we need to match to make sure 
-    //we only select level 3 categories that have the same level 1 category
-    let levelTwoString = makeStringPath(levelTwoUrl);
+  //All the objects in jobs array
+  const [jobs, setJobs] = useState([]);
+  const [alphaList, setAlphaList] = useState([]);
+  //where we set the id of the category that was clicked to sate
+  const [jobTitleList, setJobTitleList] = useState([]);
+  const [jobName, setJobName] = useState([]);
+  const [jobObj, setJobObj] = useState({
+    id: 0,
+    title: "",
+    Hierarchy: "",
+    OccupationType: "",
+    Employment2016: 0,
+    Employment2026: 0,
+    ChgEmploy2016to26Num: 0,
+    ChgEmploy2016to26Perc: 0,
+    PercentSelfEmployed2016: 0,
+    OccupationalOpenings2016to2026AnnualAverage: 0,
+    MedianAnnualWage2017: "",
+    TypicalEducationNeededForEntr: "",
+    WorkExperienceInARelatedOccupation: "",
+    TypicalOnTheJobTrainingNeededToAttainCompetencyInTheOccupation: "",
+    ttl: "",
+    Level0: "",
+    Level4: "",
+    Level3: "",
+    Level2: "",
+    Level1: "",
+    pathString: "",
+    Def: "",
+  });
+  //endpoint
+  let endpoint = props.category;
+  // the level 1 category url sting we need to pass to children
+  const levelOneUrl = props.levelOneUrl;
+  // the level 2 category url sting we need to pass to children
+  const levelTwoUrl = props.levelTwoUrl;
+  //we only select level 2 categories that have the same level 1 category
+  let levelOneString = makeStringPath(levelOneUrl);
+  //the level 2 category 'normal' string we need to match to make sure
+  //we only select level 3 categories that have the same level 1 category
+  let levelTwoString = makeStringPath(levelTwoUrl);
 
-    let endpointString = makeStringPath(endpoint);
+  let endpointString = makeStringPath(endpoint);
 
-    useEffect(() => {
-        JobManager.getAll().then((jobs) => {
-            setJobs(jobs)
-        })
-    }, []);
+  useEffect(() => {
+    JobManager.getAll().then((jobs) => {
+      setJobs(jobs);
+      setJobName(endpointString);
+    });
+  }, [endpointString]);
 
-    //empty arry to push names of target level (Leve2)
-    let levelList = []
-//    console.log(levelList, "Here it is")
-    const filterlevelThree = () => jobs.forEach((jobCategory) => {
-        let noDash = removeDash(jobCategory.Level2)
-        if (noDash === levelTwoString && jobCategory.Level3 !== "NA" && !levelList.includes(jobCategory.Level3)) {
-            levelList.push(jobCategory.Level3);
+  ////////////////////////////////////////////////////////////
+
+  useEffect(() => {
+    //Render Alphabatized selection to page
+    const filterlevelThree = (jobs) => {
+      let levelList = [];
+
+      jobs.forEach((jobCategory) => {
+        let noDash = removeDash(jobCategory.Level2);
+        if (
+          noDash === levelTwoString &&
+          jobCategory.Level3 !== "NA" &&
+          !levelList.includes(jobCategory.Level3)
+        ) {
+          levelList.push(jobCategory.Level3);
         }
-       
-    }  );
+      });
 
-    //call filter
-    filterlevelThree();  
-     //alphabitize sorted list to use when mapping array to DOM  
-    const alphaList = levelList.sort();
-    // console.log("LevelList", LevelOneList)
-
-    
-    //OBJECT 
-    //This get's the id of whatever category was clicked
-    //THis is the Functionality to get object for Table
-    useEffect(() => {
-        getClickedJobObject();
-        }, )
-            
-    const getClickedJobObject = () => {
-        const arrayHold = [];
-        jobs.forEach(function (job) {
-            arrayHold.push(jobObj.ttl === jobName)  
-            })
-
-        // console.log(arrayHold);
-        if (arrayHold.includes(true)) {
-             // console.log("Array Hold had one true value")
-            jobs.forEach((jobObj) => {
-                if(jobName === jobObj.ttl){
-                    setJobObj(jobObj)
-                }
-            })
-        } else{
-            jobs.forEach((jobObj) => {
-                let noDashTtl = removeDash(jobObj.ttl)
-                let noDashObj = removeDash(jobObj.title)
-                if(endpointString === noDashTtl|| endpointString === noDashObj){
-                    setJobObj(jobObj)
-                }
-            })
-        };     
+      let sortedList = levelList.sort();
+      setAlphaList(sortedList);
     };
+    ////Call the Function
+    filterlevelThree(jobs);
 
-       ///THis is just a test run for search bar choices
-        ///is this where I should pull all titles?
-    //Search Functionality ???????
-    let jobTitleList =[]
-    const getAllJobNames = () => jobs.forEach((job) => {
-        if (!jobTitleList.includes(job.title)) {
-            jobTitleList.push(job.title)
+    ///THis is just a test run for search bar choices
+    const getAllJobNames = (jobs) => {
+      let jobTList = [];
+      jobs.forEach((job) => {
+        if (!jobTList.includes(job.title)) {
+          jobTList.push(job.title);
         }
-        })
+      });
+      setJobTitleList(jobTList);
+    };
+    ////Call the Function
+    getAllJobNames(jobs);
+  }, [jobs, levelTwoString]);
 
-    getAllJobNames();
+  ///////////////////////////////////////////////////////////////
 
+  useEffect(() => {
+    const getClickedJobObject = () => {
+      const arrayHold = [];
+      jobs.forEach(function (jobObj) {
+        arrayHold.push(jobObj.ttl === jobName);
+      });
 
+      // console.log(arrayHold);
+      if (arrayHold.includes(true)) {
+        // console.log("Array Hold had one true value")
+        jobs.forEach((jobObj) => {
+          if (jobName === jobObj.ttl) {
+            setJobObj(jobObj);
+          }
+        });
+      } else {
+        jobs.forEach((jobObj) => {
+          let noDashTtl = removeDash(jobObj.ttl);
+          let noDashObj = removeDash(jobObj.title);
+          if (endpointString === noDashTtl || endpointString === noDashObj) {
+            setJobObj(jobObj);
+          }
+        });
+      }
+    };
+    getClickedJobObject(jobName);
+  }, [jobName, endpointString, jobs]);
 
-   return (
-        <>
-        <div>
-            <Title />
+  return (
+    <>
+      <div>
+        <Title />
+      </div>
+      <h1> PAGE I'M WORKING ON</h1>
+      <div>
+        <LrAutoSearchV2 jobs={jobs} jobTitleList={jobTitleList} {...props} />
+      </div>
+      <div className="jobviz-header">
+        <h4>{endpointString}</h4>
+      </div>
+
+      <div className="crumbs">
+        <small>
+          <Link to={"/"}>Jobs</Link> >{" "}
+          <Link to={"/job-catagories"}>Job Categories</Link> >{" "}
+          <Link to={`/job-catagories/${levelOneUrl}`}>{levelOneString}</Link> >{" "}
+          <Link to={`/job-catagories/${levelOneUrl}/${levelTwoUrl}`}>
+            {levelTwoString}
+          </Link>{" "}
+          > {endpointString}
+        </small>
+      </div>
+
+      <div className="jobviz-parent">
+        <div className="btn-container">
+          <div
+            type="button"
+            className="link-btn"
+            onClick={() => {
+              props.history.push("/");
+              // console.log("you clicked me");
+            }}
+          >
+            -
+          </div>
         </div>
-        <div>
-                <LrAutoSearchV2 jobs={jobs} jobTitleList={jobTitleList} {...props}  />
-            </div>
-        <div className="jobviz-header">    
-                
-                <h4>{endpointString}</h4>
-             
-        </div>  
-        
-        <div className="crumbs">
-                    <small><Link to={"/"}>Jobs</Link> > <Link to={"/job-catagories"}>Job Categories</Link> > <Link to={`/job-catagories/${levelOneUrl}`}>{levelOneString}</Link> > <Link to={`/job-catagories/${levelOneUrl}/${levelTwoUrl}`}>{levelTwoString}</Link> > {endpointString}</small>
-        </div>  
-        
-        <div className="jobviz-parent"> 
-            <div className="btn-container">
-                <div type="button"
-                        className="link-btn"
-                        onClick={() => {
-                        props.history.push("/");
-                        // console.log("you clicked me");
-                }}>-</div>  
-                </div>
-            <div className="btn-container">
-                <div type="button"
-                        className="link-btn"
-                        onClick={() => {
-                            props.history.push("/job-catagories");
-                            // console.log("you clicked me");
-                    }}>-</div>  
-                </div>
-            <div className="btn-container">
-                <div type="button"
-                        className="link-btn"
-                        onClick={() => {
-                            props.history.push(`/job-catagories/${levelOneUrl}`);
-                            // console.log("you clicked me");
-                }}>-</div> 
-            </div>
-                    <div className="container-cards">
-                        {alphaList.map((orderedCategory) => {
-                        return (
-                            <div key={orderedCategory} className="option"  onClick={() =>setJobName(orderedCategory)}> 
-                                <LevelThreeCard
-                                    key={orderedCategory}
-                                    orderedCategory={orderedCategory}
-                                    jobs={jobs}
-                                    levelOneUrl={levelOneUrl}
-                                    levelTwoUrl={levelTwoUrl}
-                                    {...props}
-                                />
-                            </div>
-                        )
-
-                        })}
-                    </div>
-                </div>
-                <div className="jobviz-parent">
-                    <Table jobObj={jobObj} {...props} />
-                </div>
-    
-         
-        
-        
-        
+        <div className="btn-container">
+          <div
+            type="button"
+            className="link-btn"
+            onClick={() => {
+              props.history.push("/job-catagories");
+              // console.log("you clicked me");
+            }}
+          >
+            -
+          </div>
+        </div>
+        <div className="btn-container">
+          <div
+            type="button"
+            className="link-btn"
+            onClick={() => {
+              props.history.push(`/job-catagories/${levelOneUrl}`);
+              // console.log("you clicked me");
+            }}
+          >
+            -
+          </div>
+        </div>
+        <div className="container-cards">
+          {alphaList.map((orderedCategory) => {
+            return (
+              <div
+                key={orderedCategory}
+                className="option"
+                onClick={() => {
+                  //   console.log("you clicked me");
+                  //   console.log(orderedCategory);
+                  setJobName(orderedCategory);
+                }}
+              >
+                <LevelThreeCard
+                  key={orderedCategory}
+                  orderedCategory={orderedCategory}
+                  jobs={jobs}
+                  levelOneUrl={levelOneUrl}
+                  levelTwoUrl={levelTwoUrl}
+                  {...props}
+                />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <div className="jobviz-parent">
+        <Table jobObj={jobObj} {...props} />
+      </div>
     </>
-)
-}
+  );
+};
--- src/components/search/LRautoSearchV2.js
+++ src/components/search/LRautoSearchV2.js
@@ -55,20 +55,98 @@ export const LrAutoSearchV2 = (props) => {
   };
 
   useEffect(() => {
+    const getSelectedJobObject = () => {
+      jobs.forEach(function (job) {
+        if (job.title === selectedJob) {
+          setSelectedJobObj(job);
+        }
+      });
+    };
     getSelectedJobObject();
-  }, [selectedJob]);
+  }, [selectedJob, jobs]);
 
   useEffect(() => {
-    goToJobUrl(selectedJobObj);
-  }, [selectedJobObj]);
+    const goToJobUrl = (selectedJobObj) => {
+      const jobCatagories = "job-catagories";
+      let category = "";
+      let title = selectedJobObj.title;
+      let level0 = selectedJobObj.Level0;
+      let level1 = selectedJobObj.Level1;
+      let level2 = selectedJobObj.Level2;
+      let level3 = selectedJobObj.Level3;
+      let level4 = selectedJobObj.Level4;
 
-  const getSelectedJobObject = () => {
-    jobs.forEach(function (job) {
-      if (job.title === selectedJob) {
-        setSelectedJobObj(job);
+      if (title !== "" && title === level0) {
+        // console.log(level0, `/`);
+        props.history.push(`/`);
+      } else if (
+        title !== "" &&
+        title === level1 &&
+        title !== level2 &&
+        title !== level3 &&
+        title !== level4
+      ) {
+        category = makeUrlPath(level1);
+        // console.log("Fake URL Level 1", `/${jobCatagories}/${category}`);
+        props.history.push(`/${jobCatagories}/${category}`);
+      } else if (
+        title !== "" &&
+        title === level2 &&
+        title !== level1 &&
+        title !== level3 &&
+        title !== level4
+      ) {
+        level1 = makeUrlPath(level1);
+        category = makeUrlPath(level2);
+        // console.log(
+        //   "Fake URl Level 2",
+        //   `/${jobCatagories}/${level1}/${category}/endpoint`
+        // );
+        props.history.push(`/${jobCatagories}/${level1}/${category}/endpoint`);
+      } else if (
+        title !== "" &&
+        title === level3 &&
+        title !== level1 &&
+        title !== level2 &&
+        title !== level4
+      ) {
+        level1 = makeUrlPath(level1);
+        level2 = makeUrlPath(level2);
+        category = makeUrlPath(level3);
+
+        // console.log(
+        //   "Fake URl Level 3",
+        //   `/${jobCatagories}/${level1}/${level2}/${category}/endpoint`
+        // );
+        props.history.push(
+          `/${jobCatagories}/${level1}/${level2}/${category}/endpoint`
+        );
+      } else if (
+        title !== "" &&
+        title === level4 &&
+        title !== level1 &&
+        title !== level2 &&
+        title !== level3
+      ) {
+        level1 = makeUrlPath(level1);
+        level2 = makeUrlPath(level2);
+        level3 = makeUrlPath(level3);
+        category = makeUrlPath(level4);
+
+        // console.log(
+        //   "Fake URl Level 4",
+        //   `/${jobCatagories}/${level1}/${level2}/${level3}/${category}/endpoint`
+        // );
+        props.history.push(
+          `/${jobCatagories}/${level1}/${level2}/${level3}/${category}/endpoint`
+        );
+      } else {
+        return;
       }
-    });
-  };
+    };
+
+    goToJobUrl(selectedJobObj);
+  }, [selectedJobObj, props.history]);
 
   const onKeyDown = (e) => {
     // setActiveOption(e);
@@ -94,85 +172,6 @@ export const LrAutoSearchV2 = (props) => {
   ///////////try to match url path here?
   //   console.log("Selected job", selectedJob)
   // console.log("Selected Job Obj", selectedJobObj);
-
-  const goToJobUrl = (selectedJobObj) => {
-    const jobCatagories = "job-catagories";
-    let category = "";
-    let title = selectedJobObj.title;
-    let level0 = selectedJobObj.Level0;
-    let level1 = selectedJobObj.Level1;
-    let level2 = selectedJobObj.Level2;
-    let level3 = selectedJobObj.Level3;
-    let level4 = selectedJobObj.Level4;
-
-    if (title !== "" && title === level0) {
-      // console.log(level0, `/`);
-      props.history.push(`/`);
-    } else if (
-      title !== "" &&
-      title === level1 &&
-      title !== level2 &&
-      title !== level3 &&
-      title !== level4
-    ) {
-      category = makeUrlPath(level1);
-      // console.log("Fake URL Level 1", `/${jobCatagories}/${category}`);
-      props.history.push(`/${jobCatagories}/${category}`);
-    } else if (
-      title !== "" &&
-      title === level2 &&
-      title !== level1 &&
-      title !== level3 &&
-      title !== level4
-    ) {
-      level1 = makeUrlPath(level1);
-      category = makeUrlPath(level2);
-      // console.log(
-      //   "Fake URl Level 2",
-      //   `/${jobCatagories}/${level1}/${category}/endpoint`
-      // );
-      props.history.push(`/${jobCatagories}/${level1}/${category}/endpoint`);
-    } else if (
-      title !== "" &&
-      title === level3 &&
-      title !== level1 &&
-      title !== level2 &&
-      title !== level4
-    ) {
-      level1 = makeUrlPath(level1);
-      level2 = makeUrlPath(level2);
-      category = makeUrlPath(level3);
-
-      // console.log(
-      //   "Fake URl Level 3",
-      //   `/${jobCatagories}/${level1}/${level2}/${category}/endpoint`
-      // );
-      props.history.push(
-        `/${jobCatagories}/${level1}/${level2}/${category}/endpoint`
-      );
-    } else if (
-      title !== "" &&
-      title === level4 &&
-      title !== level1 &&
-      title !== level2 &&
-      title !== level3
-    ) {
-      level1 = makeUrlPath(level1);
-      level2 = makeUrlPath(level2);
-      level3 = makeUrlPath(level3);
-      category = makeUrlPath(level4);
-
-      // console.log(
-      //   "Fake URl Level 4",
-      //   `/${jobCatagories}/${level1}/${level2}/${level3}/${category}/endpoint`
-      // );
-      props.history.push(
-        `/${jobCatagories}/${level1}/${level2}/${level3}/${category}/endpoint`
-      );
-    } else {
-      return;
-    }
-  };
 
   return (
     <React.Fragment>
