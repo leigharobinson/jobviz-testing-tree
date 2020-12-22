f0ecaac96553a86e27d776c6c0179f37ff0d5243
leigharobinson <lmarobinson24@gmail.com>
2020-12-22 12:52:31 -0600

    changed file location for api for live site

--- src/modules/JobManager.js
+++ src/modules/JobManager.js
@@ -1,10 +1,10 @@
 const remoteURL = "http://localhost:8088";
 
 export default {
-    getAll() {
-        return fetch(`${remoteURL}/jobs`).then((result) => result.json());
-      },
-    // getJob(param) {
-    //     return fetch(`${remoteURL}/jobs?title=${param}`).then((result) => result.json());
-    // }
-}
+  getAll() {
+    return fetch(`/api/convertcsv.json`).then((result) => result.json());
+  },
+  // getJob(param) {
+  //     return fetch(`${remoteURL}/jobs?title=${param}`).then((result) => result.json());
+  // }
+};
